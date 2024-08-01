52f3fda96dbd591e7635ecb41e3fbfb86025ca84
DeaKuqo <deakuqo93@gmail.com>
2024-08-01 18:56:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <h3>This is my first react app!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
